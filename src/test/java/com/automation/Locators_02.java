c3a73624c260b4ee2807ca2091cd63fbf519dd57
yahya-yusifli <yahya_yusifli@outlook.com>
2025-04-29 11:40:44 -0400

    update2

--- src/test/java/com/automation/Locators_02.java
+++ src/test/java/com/automation/Locators_02.java
@@ -10,17 +10,50 @@ import java.time.Duration;
 public class Locators_02 {
 
     public static void main(String[] args) throws InterruptedException {
+        String name = "user";
 
         System.setProperty("webdriver.chrome.driver", "/Users/yahyayusifli/Documents/chromedriver-mac-arm64/chromedriver");
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        String password = getPassword(driver);
+
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
-        driver.findElement(By.id("inputUsername")).sendKeys("user1");
-        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.id("inputUsername")).sendKeys(name);
+        driver.findElement(By.name("inputPassword")).sendKeys(password);
         driver.findElement(By.className("signInBtn")).click();
         Thread.sleep(1000);
         System.out.println(driver.findElement(By.tagName("p")).getText());
-        Assert.assertEquals(driver.findElement(By.tagName("p")).getText(),"You are successfully logged in.");
+
+        // Verify the success message is displayed after login
+        Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
+
+        // Verify the greeting header includes the logged-in user's name
+        Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(), "Hello " + name + ",");
+
+        // Locate and click the "Log Out" button to end the user session
+        driver.findElement(By.xpath("//button[text()='Log Out']")).click();
+        driver.close();
 
     }
+
+    public static String getPassword(WebDriver driver) throws InterruptedException {
+
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+        String passwordText = driver.findElement(By.cssSelector("form p")).getText();
+
+        // Please use temporary password 'rahulshettyacademy' to Login.
+        String[] passwordArray = passwordText.split("'");
+        // [0] Please use temporary password
+        // [1] rahulshettyacademy
+        // [2] to Login.
+        String tempPassword = passwordArray[1];
+
+        return tempPassword;
+    }
+
+
 }
